88e0f4f947dbb7b8d7c4e6bf66b8c32c41273a74
Nicolás Dalmás <ndalmas9@gmail.com>
2018-11-01 01:06:55 -0300

    se muestra detalle de cada usuario
    hipervinculo en cada usuario q te lleva  aotra pagina con su detalle

--- routes/web.php
+++ routes/web.php
@@ -17,11 +17,14 @@ c
 
 
 
-Route::get('/usuarios', 'UserController@index');
+Route::get('/usuarios', 'UserController@index')
+ ->name('users.index');
 
 Route::get('/usuarios/{id}', 'UserController@show')
-    ->where('id', '[0-9]+');
+    ->where('id', '[0-9]+')
+    ->name('users.show');
 
-Route::get('/usuarios/nuevo', 'UserController@create');
+Route::get('/usuarios/nuevo', 'UserController@create')
+->name('users.create');
 
 Route::get('/saludo/{name}/{nickname?}', 'WelcomeUserController');
