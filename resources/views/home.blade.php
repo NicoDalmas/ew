43331de3a7dc12be17a4a7197b35c6a3deadbb6f
Nicolás Dalmás <ndalmas9@gmail.com>
2018-11-09 00:56:38 -0300

    Autentificaciones - tutorial terminado
    https://medium.com/@cvallejo/autenticaci%C3%B3n-de-usuarios-y-roles-en-laravel-5-5-97ab59552d91
    
    se completa el tutorial, ya funciona l modulo de autorizacion pero falta acomodarlo con las vistas y permisos. Revisar semillas pq hay 2 seeder para user.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,3 +21,12 @@
     </div>
 </div>
 @endsection
+
+<!--/**
+@if(Auth::user()->hasRole('admin'))
+    <div>Acceso como administrador</div>
+@else
+    <div>Acceso usuario</div>
+@endif
+You are logged in!
+**/
