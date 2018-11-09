0247674a2e38cd85d68b15ef15d60d1113e7e93d
Nicolás Dalmás <ndalmas9@gmail.com>
2018-11-08 23:15:18 -0300

    Update resources/views/users/edit.blade.php
    estilo al formulario editar

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -3,7 +3,10 @@
 @section('title', "Crear usuario")
 
 @section('content')
-    <h1>Editar usuario</h1>
+
+    <div class="card">
+        <h4 class="card-header">Editar usuario</h4>
+        <div class="card-body">
 
     @if ($errors->any())
         <div class="alert alert-danger">
@@ -20,19 +23,24 @@
         {{ method_field('PUT') }}
         {{ csrf_field() }}
 
-        <label for="name">Nombre:</label>
-        <input type="text" name="name" id="name" placeholder="Pedro Perez" value="{{ old('name', $user->name) }}">
-        <br>
-        <label for="email">Correo electrónico:</label>
-        <input type="email" name="email" id="email" placeholder="pedro@example.com" value="{{ old('email', $user->email) }}">
-        <br>
-        <label for="password">Contraseña:</label>
-        <input type="password" name="password" id="password" placeholder="Mayor a 6 caracteres">
-        <br>
-        <button type="submit">Actualizar usuario</button>
+        <div class="form-group">
+            <label for="name">Nombre:</label>
+            <input type="text" name="name" class="form-control" id="name" placeholder="Pedro Perez" value="{{ old('name', $user->name) }}">
+        </div>
+        <div class="form-group">
+            <label for="email">Correo electrónico:</label>
+            <input type="email" name="email" class="form-control" id="email" placeholder="pedro@example.com" value="{{ old('email', $user->email) }}">
+        </div>
+        <div class="form-group">
+            <label for="password">Contraseña:</label>
+            <input type="password" name="password" class="form-control" id="password" placeholder="Mayor a 6 caracteres">
+        </div>
+        
+        <button type="submit" class="btn btn-primary">Actualizar usuario</button>
+        <a href="{{ route('users.index') }}" class="btn btn-link">Regresar al listado de usuarios</a>
     </form>
 
-    <p>
-        <a href="{{ route('users.index') }}">Regresar al listado de usuarios</a>
-    </p>
+    </div>
+    </div>
+
 @endsection
